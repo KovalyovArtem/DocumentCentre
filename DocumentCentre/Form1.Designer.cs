9b2602ead288f57417c32335c466f7d8de020fac
KovalyovArtem <70099470+KovalyovArtem@users.noreply.github.com>
2021-03-27 15:18:00 +0300

    Весь проект

---
+++ DocumentCentre/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace DocumentCentre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.предпромотрПечатиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.печатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtbx_TextPrint = new System.Windows.Forms.RichTextBox();
+            this.редактированиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выбратьШрифтToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкаСтраницыToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.BeginPrint += new System.Drawing.Printing.PrintEventHandler(this.printDocument1_BeginPrint);
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // pageSetupDialog1
+            // 
+            this.pageSetupDialog1.Document = this.printDocument1;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.редактированиеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.предпромотрПечатиToolStripMenuItem,
+            this.печатьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // предпромотрПечатиToolStripMenuItem
+            // 
+            this.предпромотрПечатиToolStripMenuItem.Name = "предпромотрПечатиToolStripMenuItem";
+            this.предпромотрПечатиToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.предпромотрПечатиToolStripMenuItem.Text = "Предпромотр печати";
+            this.предпромотрПечатиToolStripMenuItem.Click += new System.EventHandler(this.предпромотрПечатиToolStripMenuItem_Click);
+            // 
+            // печатьToolStripMenuItem
+            // 
+            this.печатьToolStripMenuItem.Name = "печатьToolStripMenuItem";
+            this.печатьToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.печатьToolStripMenuItem.Text = "Печать";
+            this.печатьToolStripMenuItem.Click += new System.EventHandler(this.печатьToolStripMenuItem_Click);
+            // 
+            // rtbx_TextPrint
+            // 
+            this.rtbx_TextPrint.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbx_TextPrint.Location = new System.Drawing.Point(0, 24);
+            this.rtbx_TextPrint.Name = "rtbx_TextPrint";
+            this.rtbx_TextPrint.Size = new System.Drawing.Size(800, 365);
+            this.rtbx_TextPrint.TabIndex = 3;
+            this.rtbx_TextPrint.Text = "";
+            // 
+            // редактированиеToolStripMenuItem
+            // 
+            this.редактированиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выбратьШрифтToolStripMenuItem,
+            this.настройкаСтраницыToolStripMenuItem1});
+            this.редактированиеToolStripMenuItem.Name = "редактированиеToolStripMenuItem";
+            this.редактированиеToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.редактированиеToolStripMenuItem.Text = "Редактирование";
+            // 
+            // выбратьШрифтToolStripMenuItem
+            // 
+            this.выбратьШрифтToolStripMenuItem.Name = "выбратьШрифтToolStripMenuItem";
+            this.выбратьШрифтToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.выбратьШрифтToolStripMenuItem.Text = "Выбрать шрифт";
+            this.выбратьШрифтToolStripMenuItem.Click += new System.EventHandler(this.шрифтToolStripMenuItem_Click);
+            // 
+            // настройкаСтраницыToolStripMenuItem1
+            // 
+            this.настройкаСтраницыToolStripMenuItem1.Name = "настройкаСтраницыToolStripMenuItem1";
+            this.настройкаСтраницыToolStripMenuItem1.Size = new System.Drawing.Size(190, 22);
+            this.настройкаСтраницыToolStripMenuItem1.Text = "Настройка страницы";
+            this.настройкаСтраницыToolStripMenuItem1.Click += new System.EventHandler(this.настройкаСтраницыToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 389);
+            this.Controls.Add(this.rtbx_TextPrint);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DocumentCentre";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem предпромотрПечатиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem печатьToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtbx_TextPrint;
+        private System.Windows.Forms.ToolStripMenuItem редактированиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выбратьШрифтToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкаСтраницыToolStripMenuItem1;
+    }
+}
+
---
+++ DocumentCentre/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocumentCentre
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        int currentPage;
+        FontDialog fd = new FontDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            rtbx_TextPrint.Font = fd.Font;
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            string currentLine;
+            float leftMargin = e.MarginBounds.Left;
+            float topMargin = e.MarginBounds.Top;
+            float rightMargin = e.MarginBounds.Right;
+            float bottomMargin = e.MarginBounds.Bottom;
+
+            float yPos = 0;
+            int nPages;
+            int nLines;
+            int i;
+
+            nLines = (int)(e.MarginBounds.Height / fd.Font.GetHeight(e.Graphics));
+            nPages = (rtbx_TextPrint.Lines.Length - 1) / nLines + 1;
+
+
+
+            i = 0;
+            while ((i < nLines) && (counter < rtbx_TextPrint.Lines.Length))
+            {
+                currentLine = rtbx_TextPrint.Lines[counter];
+
+                yPos = topMargin + i * fd.Font.GetHeight(e.Graphics);
+                e.Graphics.DrawString(currentLine, fd.Font, new SolidBrush(rtbx_TextPrint.SelectionColor), new RectangleF(leftMargin, yPos, rightMargin, bottomMargin), new StringFormat());
+
+                counter++;
+                i++;
+            }
+
+            e.HasMorePages = false;
+            if (currentPage < nPages)
+            {
+                ++currentPage;
+                e.HasMorePages = true;
+            }
+        }
+
+        private void printDocument1_BeginPrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            counter = 0;
+            currentPage = 1;
+        }
+
+        private void настройкаСтраницыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pageSetupDialog1.ShowDialog();
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.Print();
+            }
+        }
+
+        private void предпромотрПечатиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void шрифтToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                rtbx_TextPrint.Font = fd.Font;
+            }
+        }
+    }
+}
